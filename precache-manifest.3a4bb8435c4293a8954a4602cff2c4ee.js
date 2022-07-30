d40b052b0dd82b65ca4e8b316a02867c9a84698e
goyal.aman <goyal.amanrocks@gmail.com>
2022-07-30 17:36:48 +0530

    Updates

---
+++ precache-manifest.3a4bb8435c4293a8954a4602cff2c4ee.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f380d20020d3ddaea394db0e5d9fd6d",
+    "url": "/DevFolio-Porgitfolio/index.html"
+  },
+  {
+    "revision": "fcbd049c8b8d3f6fa478",
+    "url": "/DevFolio-Porgitfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e8d53513d6f88724a999",
+    "url": "/DevFolio-Porgitfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "019aba597af6ec1e8a3b",
+    "url": "/DevFolio-Porgitfolio/static/css/main.438de11b.chunk.css"
+  },
+  {
+    "revision": "6e76848f951fdb444be3",
+    "url": "/DevFolio-Porgitfolio/static/js/2.521479b2.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/DevFolio-Porgitfolio/static/js/2.521479b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcbd049c8b8d3f6fa478",
+    "url": "/DevFolio-Porgitfolio/static/js/3.5672b75d.chunk.js"
+  },
+  {
+    "revision": "e8d53513d6f88724a999",
+    "url": "/DevFolio-Porgitfolio/static/js/4.d9bdbd92.chunk.js"
+  },
+  {
+    "revision": "019aba597af6ec1e8a3b",
+    "url": "/DevFolio-Porgitfolio/static/js/main.1d05d37e.chunk.js"
+  },
+  {
+    "revision": "a3cd17d04cb343d39a29",
+    "url": "/DevFolio-Porgitfolio/static/js/runtime-main.8cab2218.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/DevFolio-Porgitfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/DevFolio-Porgitfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/DevFolio-Porgitfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/DevFolio-Porgitfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/DevFolio-Porgitfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/DevFolio-Porgitfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "7d084609dd11419c7baa48193fb587a2",
+    "url": "/DevFolio-Porgitfolio/static/media/hackveda.7d084609.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/DevFolio-Porgitfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/DevFolio-Porgitfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/DevFolio-Porgitfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/DevFolio-Porgitfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/DevFolio-Porgitfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/DevFolio-Porgitfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "c1b3c5b96ec228fb3998227c34c16dd6",
+    "url": "/DevFolio-Porgitfolio/static/media/zs-associates.c1b3c5b9.svg"
+  }
+]);
